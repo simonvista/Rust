a856011f11cdc03726387f00191188683b0dd548
YL <simon-ling@hotmail.com>
2021-09-26 21:48:47 -0400

    done

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod print_format;
 // mod variable;
 // mod data_type;
-mod strings;
+// mod strings;
+mod tuples;
 
 fn main() {
     // println!("Hello, world!");  
     // print_format::run();
     // variable::run();
     // data_type::run();
-    strings::run();
+    // strings::run();
+    tuples::run();
 }
---
+++ src/tuples.rs
@@ -0,0 +1,7 @@
+// Tuples group together values of different types
+// Max 12 elements
+
+pub fn run() {
+    let person=("Brad","Mass",22);
+    println!("name: {}, state: {}, age: {}",person.0,person.1,person.2);
+}
